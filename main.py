def3428f4d6316d23fa5341831c7ed99cc2ee4a9
Rex2626 <rexattwood26@gmail.com>
2022-06-01 16:00:25 +0100

    Started on collision

--- main.py
+++ main.py
@@ -1,12 +1,13 @@
 from time import perf_counter
 import pygame
 import sys
-from objects import Ball
+from statistics import mean
+from objects import Object, MoveableObject
 import _menu
 
 pygame.init()
 
-WIN = pygame.display.set_mode(flags=pygame.FULLSCREEN + pygame.RESIZABLE)
+WIN = pygame.display.set_mode((1600, 1000), flags=pygame.RESIZABLE)
 pygame.display.set_caption("GamingX Framework")
 WIDTH, HEIGHT = pygame.display.get_window_size()
 FULLSCREEN = True
@@ -44,12 +45,12 @@ def update_playing_screen_size(menu: "_menu.Menu"):
     "Clip the coords of any object out of bounds"
 
     
-def draw_window(objects):
+def draw_window(objects: list[Object]):
     """Draw window"""
     WIN.fill(BLACK)
 
     for object in objects:
-        object.draw()
+        object.draw(WIN)
 
     pygame.display.update()
 
@@ -61,10 +62,64 @@ def handle_player_movement(keys_pressed, objects):
         "move_up()"
 
 
-def handle_movement(objects, delta_time):
+def handle_movement(objects: list[MoveableObject], delta_time):
     """Handles movement for all objects, adjusts positions based on velocity"""
+    dt = delta_time
+
+    # Loop until every object has moved for the given time
+    while dt:
+        # Find two objects closest to collision
+        closest_time = dt
+        closest_objects = None, None
+        # For each object, find it's closest collision to every object
+        for object1 in objects:
+            for object2 in objects:
+
+                dist_between = min(object2.x - (object1.x + object1.width), (object2.x + object2.width) - object1.x) # Closest distance between two objects, adjust for width
+                rel_vel = object1.vx - object2.vx # Relative velocity between two objects
+                if rel_vel == 0: continue # Two objects will never collide if moving at same velocity
+                coll_time = dist_between / rel_vel # Time until the two objects collide
+
+                if coll_time >= 0 and coll_time < closest_time: # Check that two objects will collide and that the time is closer than closest_time
+                    closest_time = coll_time
+                    closest_objects = object1, object2
+
+        if closest_objects[0]: # If the two objects collided
+            print("hi")
+
+            # Move closest objects to collision point
+            closest_objects[0].x += closest_time * closest_objects[0].vx
+            closest_objects[1].x += closest_time * closest_objects[1].vx
+
+            # Set the two objects velocity to the average of both
+            average_velocity = mean((closest_objects[0].vx, closest_objects[1].vx))
+            closest_objects[0].vx = average_velocity
+            closest_objects[1].vx = average_velocity
+            print(average_velocity)
+            print(closest_objects[0].vx, closest_objects[1].vx)
+
+            # Move all other objects the same amount that the collision objects moved
+            for object in objects:
+                if object not in closest_objects:
+                    object.x += closest_time * object.vx
+
+            # Subtract closest_time from dt as the game time has advance by closest_time seconds
+            dt -= closest_time
+
+        # If no collision
+        else:
+            # Move objects the rest of the way, then break to move onto the y velocity
+            for object in objects:
+                object.x += dt * object.vx
+            break
+
+
     for object in objects:
-        object.update_pos(delta_time)
+        object.update_posy(delta_time)
+
+    for object in objects:
+        if object.x < 0 or object.x + object.width > 1600:
+            object.vx *= -1
 
 
 def quit():
@@ -77,8 +132,9 @@ def main(menu: "_menu.Menu"):
     """Main game loop"""
     delta_time = 0
 
-    example_object = Ball
-    objects = [example_object]
+    objects = []
+    for i in range(1, 6):
+        objects.append(MoveableObject(i * 110, 100, i * 100, i * 10, 100, 100, pygame.transform.scale(pygame.image.load("./assets/example.png"), (100, 100))))
 
     running = True
     paused = False
--- objects.py
+++ objects.py
@@ -1,212 +1,10 @@
-import random
 import pygame
 import os
 import sys
 
 
 
-class Rect():
-    def __init__(self, tlx, tly, brx, bry) -> None: #tlx = top left x etc
-        self.tlx = tlx
-        self.tly = tly
-        self.brx = brx
-        self.bry = bry
-
-    def intersects_other_rect(self, other: "Rect") -> bool:
-        if self.tlx > other.brx: return False # to the right
-        if self.brx < other.tlx: return False # to the left
-        if self.tly > other.bry: return False # below
-        if self.bry < other.tly: return False # above
-        return True
-
-    def top_l(self) -> "tuple(float, float)":
-        return (self.tlx, self.tly)
-
-    def top_r(self) -> "tuple(float, float)":
-        return (self.brx, self.tly)
-
-    def bot_l(self) -> "tuple(float, float)":
-        return (self.tlx, self.bry)
-
-    def bot_r(self) -> "tuple(float, float)":
-        return (self.brx, self.bry)
-
-    def corners(self) -> "tuple(tuple(float, float))":
-        return (self.top_l(), self.top_r(), self.bot_l(), self.bot_r())
-
-    def width(self) -> "float":
-        '''(can return negative numbers)'''
-        return self.brx - self.tlx
-
-    def height(self) -> "float":
-        '''(can return negative numbers)'''
-        return self.bry - self.tly
-
-    def draw(self, window: "pygame.Surface", colour):
-        pygame.draw.rect(window, colour, [self.tlx, self.tly, self.width(), self.height()])
-
-
-def sub_points(point_a: "tuple(float, float)", point_b: "tuple(float, float)") -> "tuple(float, float)":
-    return tuple(point_a[i] - point_b[i] for i in (0, 1))
-
-def point_sqrlength(point: "tuple(float, float)"):
-    return point[0]*point[0] + point[1]*point[1]
-
-def point_min_abs_component(point: "tuple(float, float)"):
-    return min(abs(point[i]) for i in (0, 1))
-
-def sign(x):
-    if x >= 0: return 1
-    return -1
-
-
-class Entity():
-    def __init__(self, x: float, y: float) -> None:
-        self.x = x
-        self.y = y
-
-    def update(self, dt: float, speed: float):
-        pass
-
-
-class SquareEntity(Entity):
-    def __init__(self, x, y, width, height) -> None:
-        super().__init__(x, y)
-        self.width = width
-        self.height = height
-
-    def rect(self) -> Rect:
-        return Rect(self.x, self.y, self.x + self.width, self.y + self.height)
-
-    def draw(self, window, colour):
-        self.rect().draw(window, colour)
-
-
-class Padel(SquareEntity):
-    def __init__(self, x, y, width, height) -> None:
-        super().__init__(x, y, width, height)
-        self.moving_down = False
-        self.moving_up = False
-        self.extra_height = 0
-        self.extra_height_change_rate = 0
-
-    def update(self, dt: float, speed: float):
-        # bruh rex don't remove this
-        if self.extra_height > 0 and sign(self.extra_height_change_rate) < 0 or\
-        self.extra_height < 0 and sign(self.extra_height_change_rate) > 0:
-            self.extra_height += dt*self.extra_height_change_rate
-        else:
-            self.extra_height_change_rate = 0
-            self.extra_height = 0
-
-    def get_y(self):
-        return self.y - self.extra_height/2
-
-    def get_height(self):
-        return self.height + self.extra_height
-
-    def rect(self) -> Rect:
-        return Rect(self.x, self.get_y(), self.x + self.width, self.get_y() + self.height + self.extra_height)
-
-
-class Ball(SquareEntity):
-    def __init__(self, screen_width, screen_height, ball_width, ball_height, text_bar_height) -> None:
-        super().__init__(0, 0, ball_width, ball_height)
-        self.screen_width = screen_width
-        self.screen_height = screen_height
-        self.text_bar_height = text_bar_height
-        self.restart()
-
-    def update(self, dt: float, speed: float):
-        self.move(speed)
-
-    def move(self, speed):
-        self.x += (self.vx + self.spinx) * speed
-        self.y += (self.vy + self.spiny) * speed
-
-        if self.spinx > 0:
-            self.spinx -= min(self.spinx, speed / self.screen_width)
-        elif self.spinx < 0:
-            self.spinx += min(-self.spinx, speed / self.screen_width)
-
-        if self.spiny > 0:
-            self.spiny -= min(self.spiny, 2 * speed / self.screen_width)
-        elif self.spiny < 0:
-            self.spiny += min(-self.spiny, 2 * speed / self.screen_width)
-
-    def handle_paddle_collisions(self, padel: Padel, spin: bool):
-        sr = self.rect()
-        pr = padel.rect()
-        collides_with_padel = sr.intersects_other_rect(pr) # check we do have a collision
-        if not collides_with_padel: return
-
-        # this finds the vector from a corner of the paddle to
-        # the corner of the ball, with the smallest length of
-        # it's minimum component i.e. the one which enforces the
-        # smallest change on the ball's position in order to
-        # resolve the collision
-        intersection_vector = min(
-            sub_points(sr.bot_r(), pr.top_l()),
-            sub_points(sr.bot_l(), pr.top_r()),
-            sub_points(sr.top_r(), pr.bot_l()),
-            sub_points(sr.top_l(), pr.bot_r()),
-        key=point_min_abs_component)
-
-
-        if abs(intersection_vector[0]) < abs(intersection_vector[1]): # horizontal bounce
-            dist_to_centre = (self.y + self.height / 2) - (padel.get_y() + padel.get_height() / 2) # Distance from center of ball to centre of padel
-            self.vy = dist_to_centre / padel.get_height() # move vertically more quickly when close to edges of paddle
-            self.vx = max(1 - self.vy**2, 0.1)**0.5 * -sign(self.vx) # set vx such that the total vel is 1 and so that it is in the right direction
-            # shift to the inverse of the intersection vector to escape collision
-            self.x -= intersection_vector[0]
-            # handle spin
-            if spin:
-                if padel.moving_up:
-                    self.spiny += 1
-                    self.spinx += 0.5*sign(self.vx)
-                elif padel.moving_down:
-                    self.spiny -= 1
-                    self.spinx += 0.5*sign(self.vx)
-
-        else: # "side" (vertical) bounce
-            self.vy *= -1 # invert y vel
-            if not self.ball_has_hit_side and (padel.moving_down or padel.moving_up): # make sure only increasing vy once, otherwise vy could increase exponentially
-                self.vy *= 2 # multiply by 2 to make the effect more visible
-                self.ball_has_hit_side = True
-            self.spiny = 0 # to avoid bouncing back into the paddle
-            # shift to the inverse of the intersection vector to escape collision
-            self.y -= intersection_vector[1]
-            if self.y + self.width > self.screen_height or self.y < self.text_bar_height:
-                self.y -= padel.get_height() + self.height
-
-    def boundary_collision(self):
-        if self.y < self.text_bar_height + 2:
-            self.vy *= -1
-            self.spiny *= -1
-            delta = self.y - (self.text_bar_height + 2)
-            self.y -= delta
-        elif self.y + self.height > self.screen_height - 2:
-            self.vy *= -1
-            self.spiny *= -1
-            delta = (self.y + self.height) - (self.screen_height - 2)
-            self.y -= delta
-
-    def collides_with_paddle_check(self, padel: Padel):
-        return self.rect().intersects_other_rect(padel.rect())
-
-    def restart(self):
-        self.x = self.screen_width / 2 - self.width / 2
-        self.y = random.randint(self.text_bar_height + 2, self.screen_height - self.height - 2)
-        self.spinx = 0
-        self.spiny = 0
-        vx = random.random() * 0.8 + 0.2
-        vy = 1 - vx
-        self.vx, self.vy = vx**0.5, vy**0.5
-        self.vx *= random.randint(0, 1) * 2 - 1
-        self.ball_has_hit_side = False
-
-
-def resource_path(relative_path):
+def get_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
@@ -215,3 +13,30 @@ def resource_path(relative_path):
         base_path = os.path.abspath(".")
 
     return os.path.join(base_path, relative_path)
+
+
+
+class Object():
+    def __init__(self, x, y, width, height, image) -> None:
+        self.x = x
+        self.y = y
+        self.width = width
+        self.height = height
+        self.image = image
+
+    def draw(self, win: pygame.Surface):
+        win.blit(self.image, (round(self.x), round(self.y)))
+
+
+
+class MoveableObject(Object):
+    def __init__(self, x, y, vx, vy, width, height, image) -> None:
+        super().__init__(x, y, width, height, image)
+        self.vx = vx
+        self.vy = vy
+
+    def update_posx(self, delta_time):
+        self.x += self.vx * delta_time
+    
+    def update_posy(self, delta_time):
+        self.y += self.vy * delta_time
